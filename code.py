0f3728ba7041b2098ce2eb6b3920f1bcf495efcf
yonghenglh6 <yonghenglh6@163.com>
2016-04-22 03:05:02 +0800

    add setting page

--- code.py
+++ code.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
 import web
-
+import pickle
+import os
 urls = ("/", "index",
         "/biaoding\.html", "biaoding",
         "/help\.html", "help",
@@ -42,8 +43,33 @@ class hotmap:
 
 class setting:
     def GET(self):
-        return render.setting()
+        return render.setting(TaskManager.get())
+    def POST(self):
+        i = web.input(action="none", traindir="", predictdir="", background_picture="", rows="", columns="");
+        if i.action=="newtask":
+            ntask={"traindir":i.traindir, "predictdir":i.predictdir, "background_picture":i.background_picture, "rows":i.rows, "columns":i.columns};
+            TaskManager.add(ntask);
+        return render.setting(TaskManager.get());
 
-
+class MTaskManager:
+    def __init__(self):
+        self.filename="tasks.list";
+        if not os.path.exists(self.filename):
+            self.tasks=[];
+            with open(self.filename, 'w') as f:
+                pickle.dump(self.tasks, f)
+        else:
+            with open(self.filename,'r') as f:
+                self.tasks=pickle.load(f);
+    def add(self,ntask):
+        self.tasks.append(ntask);
+        with open(self.filename, 'w') as f:
+            pickle.dump(self.tasks, f)
+    def get(self,id=-1):
+        if id==-1:
+            return self.tasks;
+        else:
+            return self.tasks[id]
+TaskManager= MTaskManager();
 if __name__ == "__main__":
     app.run()
